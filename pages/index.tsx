9e553e5059c5ba606a9b36906d2f0e761bfc4762
islam bouterbiat <islambouter2000@gmail.com>
2022-03-29 18:22:14 +0100

    created NavBar content + background

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 
 const IndexPage = () => (
-  <main title="Home | Next.js + TypeScript Example" className="">
+  <main title="Home | Next.js + TypeScript Example" className="container mx-auto bg-violet-600/5 text-white">
     <h1>Hello Next.js 👋</h1>
     <p>
       <Link href="/about">
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  content: [],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontWeight: {
@@ -24,6 +27,7 @@ module.exports = {
         'blue-sky': '#01B7F0',
         'green-success': '#216716',
         'red-rejected': '#FF4E4E',
+        'overlay-background': 'rgba(50, 39, 115, 0.66)',
       },
     },
   },
