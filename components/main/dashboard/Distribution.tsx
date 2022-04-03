b91b279771caba1945512a988785da109c0429bd
islam bouterbiat <islambouter2000@gmail.com>
2022-04-03 21:10:07 +0100

    updated Pie Chart background colors + updating balance / value toggle

--- components/main/dashboard/Distribution.tsx
+++ components/main/dashboard/Distribution.tsx
@@ -22,14 +22,14 @@ const Distribution = () => {
         label: '# of Votes',
         data: [12, 19, 3],
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
+          'rgba(111, 159, 198, 1)',
+          'rgba(243, 168, 101, 1)',
+          'rgba(127, 185, 115, 1)',
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
+          'rgba(111, 159, 198, 1)',
+          'rgba(243, 168, 101, 1)',
+          'rgba(127, 185, 115, 1)',
         ],
         borderWidth: 1,
       },
@@ -42,7 +42,7 @@ const Distribution = () => {
         <h1 className="text-xl font-medium">Distribution</h1>
       </nav>
       <h2 className="py-3 text-sm">FI25 Crypto Index Token</h2>
-      <div className="">
+      <div className="h-32">
         <Pie data={pie_chart_data} options={options} />
       </div>
       <div className="flex justify-end">
@@ -50,14 +50,20 @@ const Distribution = () => {
           htmlFor="value_balance"
           className="relative mt-2 flex cursor-pointer items-center hover:scale-105"
         >
-          <input
-            // onClick={handleBalanceToggle}
-            type="checkbox"
-            id="value_balance"
-            className="sr-only cursor-pointer"
-          />
-          <div className="toggle_bg h-5 w-8 cursor-pointer rounded-full border-2 border-gray-200 bg-transparent"></div>
-          <span className="ml-3 cursor-pointer text-sm font-medium">
+          <span className="mr-1.5 cursor-pointer text-sm font-medium">
+            Value
+          </span>
+          <div className="relative">
+            <input
+              // onClick={handleBalanceToggle}
+              type="checkbox"
+              id="value_balance"
+              className="sr-only cursor-pointer"
+            />
+            <div className="toggle_bg h-5 w-8 cursor-pointer rounded-full border-2 border-gray-200 bg-transparent"></div>
+          </div>
+
+          <span className="ml-1.5 cursor-pointer text-sm font-medium">
             Balance
           </span>
         </label>
