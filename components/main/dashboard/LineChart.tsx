4d336cff8b5cdb1ed368b31ad49be42af31ff2e5
islam bouterbiat <islambouter2000@gmail.com>
2022-04-04 12:47:40 +0100

    Added timeframe to the Line chart (1h,1d,1w,1m,1y)

--- components/main/dashboard/LineChart.tsx
+++ components/main/dashboard/LineChart.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {
   Chart as ChartJS,
   ArcElement,
@@ -9,16 +9,19 @@ import {
   TimeScale,
   PointElement,
   LineElement,
+  BarElement,
   Title,
 } from 'chart.js'
-import { Line } from 'react-chartjs-2'
+import { Line, Bar } from 'react-chartjs-2'
 import 'chartjs-adapter-date-fns'
 
 ChartJS.register(
+  ArcElement,
   CategoryScale,
   LinearScale,
   PointElement,
   LineElement,
+  BarElement,
   Title,
   Tooltip,
   Legend,
@@ -26,6 +29,8 @@ ChartJS.register(
 )
 
 const LineChart = () => {
+  // state for giving user the ability to set chart timeframe by month or week ...
+  const [timeframe, setTimeframe] = useState('month')
   const options = {
     responsive: true,
     maintainAspectRatio: false,
@@ -43,10 +48,14 @@ const LineChart = () => {
     },
     scales: {
       x: {
+        // parsing:false we will use this when we get timestamps from api for better performance
         type: 'time',
         time: {
-          unit: 'day',
+          unit: timeframe,
         },
+      },
+      y: {
+        max: 200,
       },
     },
   }
@@ -67,12 +76,12 @@ const LineChart = () => {
         tension: 0.3,
         label: 'FI25',
         data: [
-          { x: '2022-03-12', y: 50 },
-          { x: '2022-03-23', y: 10 },
-          { x: '2022-04-12', y: 20 },
-          { x: '2022-04-28', y: 100 },
-          { x: '2022-05-12', y: 60 },
-          { x: '2022-06-12', y: 40 },
+          { x: '2022-03-12T13:00:00', y: 50 },
+          { x: '2022-03-23T14:00:00', y: 10 },
+          { x: '2022-04-12T03:00:00', y: 20 },
+          { x: '2022-04-28T18:00:00', y: 100 },
+          { x: '2022-05-12T19:00:00', y: 60 },
+          { x: '2022-06-12T10:00:00', y: 40 },
         ],
         // borderWidth: 4,
         borderColor: 'rgb(238,130,238)',
@@ -82,12 +91,12 @@ const LineChart = () => {
         tension: 0.3,
         label: 'FI10',
         data: [
-          { x: '2022-03-12', y: 100 },
-          { x: '2022-03-23', y: 80 },
-          { x: '2022-04-12', y: 70 },
-          { x: '2022-04-28', y: 10 },
-          { x: '2022-05-12', y: 20 },
-          { x: '2022-06-12', y: 70 },
+          { x: '2022-03-12T13:00:00', y: 100 },
+          { x: '2022-03-23T14:00:00', y: 80 },
+          { x: '2022-04-12T03:00:00', y: 70 },
+          { x: '2022-04-28T18:00:00', y: 10 },
+          { x: '2022-05-12T19:00:00', y: 20 },
+          { x: '2022-06-12T10:00:00', y: 70 },
         ],
         borderColor: 'rgb(55, 125, 255)',
         backgroundColor: 'rgba(55, 125, 255, 0.5)',
@@ -95,14 +104,39 @@ const LineChart = () => {
       {
         tension: 0.3,
         label: 'MetaFI',
-        data: [100, 250, 170, 240, 260, 250, 375],
+        data: [
+          { x: '2022-03-12T13:00:00', y: 200 },
+          { x: '2022-03-23T14:00:00', y: 80 },
+          { x: '2022-04-12T03:00:00', y: 100 },
+          { x: '2022-04-28T18:00:00', y: 30 },
+          { x: '2022-05-12T19:00:00', y: 20 },
+          { x: '2022-06-12T10:00:00', y: 10 },
+        ],
         borderColor: 'rgb(240, 157, 1)',
         backgroundColor: 'rgba(240, 157, 1, 0.5)',
       },
     ],
   }
+  const timeframeData = [
+    { name: 'hour', text: '1H' },
+    { name: 'day', text: '1D' },
+    { name: 'week', text: '1W' },
+    { name: 'month', text: '1M' },
+    { name: 'year', text: '1Y' },
+  ]
   return (
-    <div className="h-[15rem]">
+    <div className="relative h-[15rem]">
+      <div className="absolute top-2.5 left-12 flex items-center gap-2">
+        {timeframeData.map((t, index) => (
+          <button
+            key={index}
+            className="rounded bg-black/50 px-2 py-1 text-center text-xs text-white"
+            onClick={() => setTimeframe(t.name)}
+          >
+            {t.text}
+          </button>
+        ))}
+      </div>
       <Line options={options} data={line_chart_data} />
     </div>
   )
